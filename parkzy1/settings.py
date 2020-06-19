776923167e92314482ecb27902a4d5f4c8e170e5
Nishant Kumar <mailittonishantkumar@gmail.com>
2020-06-19 21:15:48 +0530

    django_heroku

--- parkzy1/settings.py
+++ parkzy1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -121,7 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticwriteitt')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
@@ -139,3 +139,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('hostemail')
 EMAIL_HOST_PASSWORD = os.environ.get('hostpass')
 
+django_heroku.settings(locals())
