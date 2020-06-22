019530e4a9302194b21fdb0909612310fa568768
Nishant Kumar <mailittonishantkumar@gmail.com>
2020-06-23 03:35:53 +0530

    date-posted

--- parkzy1/settings.py
+++ parkzy1/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+puabv*&xr!cyw$nu%*!p&=!@xrx!-4gqnx&+0w!__$wzspytw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['writeitt.herokuapp.com']
 
